c03b7b3a9f2d7b9caadcfa7dcfbf49e6f3786169
Robodlan <reinaldo.robles@icloud.com>
2024-06-16 20:49:28 -0400

    has pass

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getAverage(scores){
     }
     return Math.floor(avg)
 }
-console.log(getAverage([87,98,55,44,22,33,44]));
+
 
 function getGrade(score){
  if (score === 100){
@@ -22,8 +22,16 @@ function getGrade(score){
     return "F";
  }
 };
-console.log(getGrade(100));
-console.log(getGrade(90));
-console.log(getGrade(77));
-console.log(getGrade(80));
-console.log(getGrade(59));
+
+function hasPassingGrade(score){
+  if (getGrade(score) !== "F"){
+    return true
+  } else {
+    return "Failed"
+  }
+};
+
+console.log(hasPassingGrade(90));
+console.log(hasPassingGrade(77));
+console.log(hasPassingGrade(60));
+console.log(hasPassingGrade(40));
